d2376083dfc2e34403075db8b224cce60d514a7f
Remzes <victor_s1998@mail.ru>
2018-06-19 06:55:13 +0300

    Implemented memoization for search box

--- src/components/TableComponent/TableComponent.js
+++ src/components/TableComponent/TableComponent.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import ReactTable from 'react-table'
 import {connect} from 'react-redux'
 import * as actions from '../../actions'
-import {pokemonsFilter} from '../../selectors'
+import {pokemonsFilterSelector} from '../../selectors'
 
 const columns = [{
     Header: 'Name',
@@ -30,7 +30,6 @@ class TableComponent extends Component {
 
     render() {
         const {fetched, data} = this.props.pokemons;
-        console.log(this.props.pokemons)
         if (fetched) {
             return (
               <div>
@@ -43,4 +42,4 @@ class TableComponent extends Component {
     }
 }
 
-export default connect((state) => ({pokemons: pokemonsFilter(state)}), actions)(TableComponent)
+export default connect((state) => ({pokemons: pokemonsFilterSelector(state)}), actions)(TableComponent)
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,5 +1,10 @@
-export function pokemonsFilter({pokemons, filters}) {
+import {createSelector} from 'reselect'
+
+const getFilters = state => state.filters;
+const getPokemons = state => state.pokemons;
+
+export const pokemonsFilterSelector = createSelector(getPokemons, getFilters, (pokemons, filters) => {
     const {searchBoxValue} = filters;
     const filteredPokemons = pokemons.data.filter(pokemon => pokemon.name.includes(searchBoxValue));
     return {...pokemons, data: filteredPokemons}
-}
+});
